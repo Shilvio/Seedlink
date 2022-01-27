675b828da3b8f3ac35d924f46624184a255152bf
Shilvio <St3llaccia>
2022-01-27 15:27:46 +0100

    removed interrupt, added rotary encoder functions, fixed comments

--- sketch.ino
+++ sketch.ino
@@ -3,18 +3,10 @@
 #include <dht.h>
 #include <RTClib.h>
 
-//MANOVELLA
-#define ENCODER_CLK 2
-#define ENCODER_DT  3
-#define ENCODER_BTN 4
-
-//COSTANTI GLOBALI
-#define MAX_OPTION_LENGTH 14
-
-//VARIABILI GLOBALI
-bool cursorPos = 0;
-int selected_menu = 0;
-int selected_item = 0;
+//pin for rotary encoder
+#define CLK 2
+#define DT 4
+#define SW 3 //rotary encoder switch
 
 //LCD settings
 LiquidCrystal_I2C lcd(0x27,16,2); //lcd settings
@@ -24,39 +16,40 @@ const float BETA = 3950; // should match the Beta Coefficient of the thermistor 
 
 //RTC settings
 RTC_DS1307 rtc; // setting rtc to appropriatwe model
-char week[7][4] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+
 
 //Humidity sensor settings
 dht DHT;
 #define DHT22_PIN 5
 
-//INTERRUPT
 
-void rotatePin()
-{
-  int dtValue = digitalRead(ENCODER_DT);
+//Costants
+#define DEFAULT_RESPONSE_TIME 50
+#define LONG_PRESS 1000
 
-  if (dtValue == HIGH && cursorPos == 0) {
-    selected_menu++; // Clockwise
-  }
-  if (dtValue == LOW && cursorPos == 1) {
-    selected_menu--; // Counterclockwise
-  }
-}
-
-void btnPress()
-{
-  Serial.println("press()");
-}
+//global variables
+int8_t lastState = HIGH;
+unsigned long timeNow = 0;
+char week[7][4] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+unsigned long lastButtonPress = 0;
+bool singlePress= false;
+bool encoderPos;
 
 void setup() {
 
-  //setting up lcd
+  //setup lcd
   lcd.init();
   lcd.backlight();
 
   //starting serial
   Serial.begin(115200);
+
+  //setup rotary encoder
+  pinMode(CLK, INPUT);
+  pinMode(DT, INPUT);
+  pinMode(SW, INPUT_PULLUP);
+  encoderPos = digitalRead(DT);
+  
 
   //starting rtc and checking if is new
   if (! rtc.begin()) {
@@ -69,47 +62,48 @@ void setup() {
     rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
   }
 
-  pinMode(ENCODER_CLK, INPUT);
-  pinMode(ENCODER_DT, INPUT);
-  pinMode(ENCODER_BTN, INPUT_PULLUP);
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  attachInterrupt (digitalPinToInterrupt (ENCODER_CLK), rotatePin, CHANGE);
-  attachInterrupt (digitalPinToInterrupt (ENCODER_BTN), btnPress, FALLING);
-
 }
 
 void loop() {
 
-  renderMenu(selected_menu);
+  defaultScreen ();
 
-  //Premi il pulsante e guarda che succede
-  if (digitalRead(ENCODER_BTN) == HIGH) {
-    cursorPos = !cursorPos;
-  }
 }
 
-// ---------------- SELETTORE SCHERMATE ----------------- //
+//function that makes the button only pressed once
+void press(){
 
-void renderMenu(int menu)
-{
-  switch (menu)
-  {
-    case 1:
-      Serial.println(menu);
-      mainMenu();
-    break;
-    default:
-      Serial.println(menu);
-      defaultScreen();
+  unsigned long startTime;
+  int8_t state = digitalRead(SW);
+  
+  if(state != lastState){
+
+    if(state == LOW) {
+      lastButtonPress = millis(); //start a timer for detecting long presses
+      singlePress = true;
+    }
+    //detects short press
+    if( state == HIGH && millis() - lastButtonPress < LONG_PRESS ){
+      Serial.println("si abbassa il pipo");
+    }
+    
   }
+
+  //detects long press
+  if( singlePress == true & state == LOW && millis() - lastButtonPress >= LONG_PRESS ){
+    Serial.println("si alza il pipo");
+    singlePress = false;
+    
+  }
+
+  lastState = state;
+
 }
-
-
-// --------------- DEFINIZIONE SCHERMATE ----------------- //
-
-//Schermata principale - mostra temperatura ed umidità
+ 
+//main menu using millis()
 void defaultScreen () {
+
+  timeNow = millis();
 
   DateTime now = rtc.now();//set current time to a variable
   //print current day
@@ -144,27 +138,55 @@ void defaultScreen () {
   float celsius = 1 / (log(1 / (1023. / analogValue - 1)) / BETA + 1.0 / 298.15) - 273.15;
   lcd.setCursor(12,1);
   lcd.print(celsius);
+  press(); // detects button press
 
-  delay(200);
+  while(millis() < timeNow + DEFAULT_RESPONSE_TIME){
+    
+  }
 }
 
-//Menu - per selezionare altri schermi
+//main menu for selecting settings or irrigation
 void mainMenu () {
-  
-  char arr[][MAX_OPTION_LENGTH] =
-  { "Watering",
-    "Settings"
-  };
 
-  lcd.setCursor(0,cursorPos);
-  lcd.print('>');
+  timeNow = millis();
 
-  int i = 0;
+  DateTime now = rtc.now();//set current time to a variable
+  lcd.setCursor(1,0);
+  lcd.print("Watering");
+  lcd.setCursor(1,1);
+  lcd.print("Settings");
 
-  lcd.setCursor(2,0);
-  lcd.print(arr[i]);
-  lcd.setCursor(2,1);
-  lcd.print(arr[i+1]);
-  
-  //delay(200);
+  while(millis() < timeNow + DEFAULT_RESPONSE_TIME){
+    
+  }
 }
+//encoder rotation
+void rotateEncoder()
+{
+  int dtValue = digitalRead(DT);
+
+  if (dtValue == HIGH && encoderPos == 0) {
+    Serial.print("Clockwise"); // Clockwise
+  }
+  if (dtValue == LOW && encoderPos == 1) {
+    Serial.print("counter clockwise"); // Counterclockwise
+  }
+
+}
+
+//menu renderer
+void renderMenu(int menu)
+{
+  switch (menu)
+  {
+    case 1:
+      Serial.println(menu);
+      mainMenu();
+    break;
+    default:
+      Serial.println(menu);
+      defaultScreen();
+  }
+}
+
+
